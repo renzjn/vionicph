35483fee152afe85677183e248bfd2d5628df03a
renzjn <renzjn.83@gmail.com>
2018-03-30 01:15:54 +0800

    v1.1
    v1.1

--- custom-css/templates/clever/html/com_hikashop/address/address_template.php
+++
@@ -1,9 +0,0 @@
-<?php
- defined('_JEXEC') or die('Restricted access');
-?>
-{address_company}
-{address_title} {address_firstname} {address_lastname}
-{address_street}
-{address_post_code} {address_city} {address_state}
-{address_country}
-<?php echo JText::sprintf('TELEPHONE_IN_ADDRESS','{address_telephone}');?>
